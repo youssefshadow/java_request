60f6215c0203474fec89323bdda19b031ef33f7d
youssefshadow <youssef20ben@gmail.com>
2023-06-07 14:20:19 +0200

    first request add user

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
+        User test = new User("youssef", "ben", "youssef20ben@gmail.com", "Youssef93");
+        System.out.println("Le compte :"+Request.addUser(test).getNom()+"à bein été ajouté en bdd");
 
     }
 }
--- src/Request.java
+++ src/Request.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Request {
     //Attribut paramètre BDD
@@ -9,6 +7,7 @@ public class Request {
     static final String PASSWORD = "";
     //Connexion à la BDD
     private static Connection connexion;
+    //Connexion à la BDD
     static {
         try {
             connexion = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
@@ -16,4 +15,36 @@ public class Request {
             throw new RuntimeException(e);
         }
     }
+    // Methode ajouter un user
+    public static User addUser(User user){
+        User userAdd = null;
+        try{
+            // 1 Connection à la BDD...
+            Statement stmt = connexion.createStatement();
+            // 2 requête SQL
+            String sql = "INSERT INTO users (nom, prenom, email, mdp) " + "VALUES (?, ?, ?, ?)";
+            //3 préparer la requête
+            PreparedStatement preparedStatement = connexion.prepareStatement(sql);
+            // 4 Binder les paramètres
+            preparedStatement.setString(1, user.getNom());
+            preparedStatement.setString(2, user.getPrenom());
+            preparedStatement.setString(3, user.getEmail());
+            preparedStatement.setString(4, user.getMdp());
+            //5 Exécution de la requête le typage int pour savoir combien d'enregistrement
+            int addRows = preparedStatement.executeUpdate();
+            //6 Test si la requête est bien passée
+            if(addRows >0){
+                userAdd = new User(user.getNom(), user.getPrenom(), user.getEmail(), user.getMdp());
+            }
+            //7 Fermer la connexion à la BDD
+            stmt.close();
+            connexion.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return userAdd;
+    }
+
+
 }
