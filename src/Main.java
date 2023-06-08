186610b73081c7c00c593916c42cd787f6db8d02
youssefshadow <youssef20ben@gmail.com>
2023-06-08 12:07:06 +0200

    manipulate date with scanner

--- src/Main.java
+++ src/Main.java
@@ -3,47 +3,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //instancier un objet User
-        User test = new User("Akagami", "Shanks", "shanks6@test.com", "5678");
-        User test1 = new User("Yoann", "Depp", "yoann@test1.com", "51234");
-        User monTest = new User("Test1", "Test1", "allo@test1.com", "1234");
-        //Appel de la fonction getUserByMail
-        User getUser = Request.getUserByMail(test);
-        //test si un compte existe
-        if (getUser !=null){
-            System.out.println("Le compte : \n-"+getUser.getId()+" "+getUser.getEmail()+" existe déja en BDD");
-        }
-        //sinon pas de compte
-        else{
-            System.out.println("Le compte : "+Request.addUser(test1).getNom()+" a été ajouté en BDD");
-        }
-        List<User> userList = Request.getAllUsers();
-
-        System.out.println("Les comptes sont :");
-        for (User user : userList) {
-            System.out.println("- Nom : " + user.getNom());
-            System.out.println("  Prénom : " + user.getPrenom());
-            System.out.println("  Email : " + user.getEmail());
-            System.out.println("  Mot de passe : " + user.getmdp());
-            System.out.println("________");
-        }
-        // Test methode
-        User userToUpdate = new User();
-        userToUpdate.setId(2);
-
-
-        User updatedUser = Request.updateUserAttribute(userToUpdate, "prenom", "Mith");
-
-        if (updatedUser != null) {
-            System.out.println("Le 'nom' a été mis à jour  avec l'ID : " + updatedUser.getId());
-        } else {
-            System.out.println("Impossible de mettre à jour l'attribut séléctionné " );
-        }
-
-        // Scanner début
         Scanner scanner = new Scanner(System.in);
 
-        // Demander à l'utilisateur de saisir les informations
+        System.out.println("Veuillez choisir une méthode (1/2/3) :");
+        int choix = scanner.nextInt();
+
+        switch (choix) {
+            case 1:
+                addUser();
+                break;
+            case 2:
+                updateUser();
+                break;
+            case 3:
+                getAllUsers();
+                break;
+            default:
+                System.out.println("Demande invalide !!!.");
+        }
+    }
+
+    private static void addUser() {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Entrez votre nom :");
         String nom = scanner.nextLine();
 
@@ -56,21 +38,52 @@ public class Main {
         System.out.println("Entrez votre mot de passe :");
         String mdp = scanner.nextLine();
 
-        // Construire un objet User
         User user = new User(nom, prenom, email, mdp);
 
-        // Vérifier si le compte existe déjà
         User exist = Request.getUserByMail(user);
 
         if (exist == null) {
-            // Le compte n'existe pas
             User addedUser = Request.addUser(user);
             System.out.println("Le compte a été ajouté : " + addedUser.getPrenom());
         } else {
-            // Le compte existe déjà
-            System.out.println("Erreur : Le compte existe déjà pour l'email " + email);
+            System.out.println("Erreur : Le compte existe déjà pour l'email " + user.getEmail());
         }
+    }
 
+    private static void updateUser() {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Entrez l'ID de l'utilisateur à mettre à jour :");
+        int userId = scanner.nextInt();
+
+        User userToUpdate = new User();
+        userToUpdate.setId(userId);
+
+        System.out.println("Entrez le nom de l'attribut à mettre à jour (nom, prenom, email, mdp) :");
+        String attributeName = scanner.next();
+
+        System.out.println("Entrez la nouvelle valeur :");
+        String newValue = scanner.next();
+
+        User updatedUser = Request.updateUserAttribute(userToUpdate, attributeName, newValue);
+
+        if (updatedUser != null) {
+            System.out.println("Attribut mis à jour avec succès : " + attributeName);
+        } else {
+            System.out.println("Impossible de mettre à jour l'attribut.");
+        }
+    }
+
+    private static void getAllUsers() {
+        List<User> userList = Request.getAllUsers();
+
+        System.out.println("Les comptes sont :");
+        for (User user : userList) {
+            System.out.println("- Nom : " + user.getNom());
+            System.out.println("  Prénom : " + user.getPrenom());
+            System.out.println("  Email : " + user.getEmail());
+            System.out.println("  Mot de passe : " + user.getmdp());
+            System.out.println("________");
+        }
     }
 }
